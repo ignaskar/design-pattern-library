6cb8779caad1f68a9c9d1902f4cd3c6ff07875e9
Ignas Karpusenkovas <ignas.karpusenkovas@gmail.com>
2021-01-02 21:20:55 +0200

    Demo 2 - Factory Method Pattern
    Implemented Factory Method Pattern by creating an abstract ShippingProviderFactory.

---
+++ src/Factory/Demo 2 - Factory Method Pattern/Factory Method Pattern/Business/Models/Shipping/Factories/ShippingProviderFactory.cs
@@ -0,0 +1,27 @@
+﻿namespace Factory_Method_Pattern.Business.Models.Shipping.Factories
+{
+    public abstract class ShippingProviderFactory
+    {
+        public abstract ShippingProvider CreateShippingProvider(string country);
+
+        public ShippingProvider GetShippingProvider(string country)
+        {
+            var provider = CreateShippingProvider(country);
+
+            /*
+             * Example of why you should create a separate GetShippingProvider method
+             * instead of using just the CreateShippingProvider
+             *
+             * if(country == "Sweden" &&
+             *     provider.InsuranceOptions.ProviderHasInsurance)
+             * {
+             *     provider.RequireSignature = false;
+             * }
+             *
+             * Basically, change something before it is returned to the caller.
+             */
+
+            return provider;
+        }
+    }
+}
---
+++ src/Factory/Demo 2 - Factory Method Pattern/Factory Method Pattern/Business/ShoppingCart.cs
@@ -0,0 +1,27 @@
+﻿using Factory_Method_Pattern.Business.Models.Commerce;
+using Factory_Method_Pattern.Business.Models.Shipping;
+using Factory_Method_Pattern.Business.Models.Shipping.Factories;
+
+namespace Factory_Method_Pattern.Business
+{
+    public class ShoppingCart
+    {
+        private readonly Order order;
+        private readonly ShippingProviderFactory shippingProviderFactory;
+
+        public ShoppingCart(Order order, ShippingProviderFactory shippingProviderFactory)
+        {
+            this.order = order;
+            this.shippingProviderFactory = shippingProviderFactory;
+        }
+
+        public string Finalize()
+        {
+            var shippingProvider = shippingProviderFactory.GetShippingProvider(order.Sender.Country);
+
+            order.ShippingStatus = ShippingStatus.ReadyForShippment;
+
+            return shippingProvider.GenerateShippingLabelFor(order);
+        }
+    }
+}
