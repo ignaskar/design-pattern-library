df16ee08a9e1d9d8bc8e94a5e3eeccd46abea6e1
Ignas Karpusenkovas <ignas.karpusenkovas@gmail.com>
2021-01-03 18:56:08 +0200

    Implement Singleton
    Implemented different variatons of singleton design pattern.

---
+++ src/Singleton/Singleton/v2_Locking/Singleton.cs
@@ -0,0 +1,28 @@
+﻿namespace SingletonPattern.Singleton.v2_Locking
+{
+    // Bad code
+    // Source: https://csharpindepth.com/articles/singleton
+    public sealed class Singleton
+    {
+        private static Singleton _instance = null;
+        private static readonly object padlock = new object();
+
+        public static Singleton Instance
+        {
+            get
+            {
+                Logger.Log("Instance called.");
+                lock (padlock) // this lock is used on *every* reference to Singleton
+                {
+                    return _instance ??= new Singleton();
+                }
+            }
+        }
+
+        private Singleton()
+        {
+            // cannot be created except within this class
+            Logger.Log("Constructor invoked.");
+        }
+    }
+}
---
+++ src/Singleton/Singleton/v4_LessLazy/Singleton.cs
@@ -0,0 +1,32 @@
+﻿namespace SingletonPattern.Singleton.v4_LessLazy
+{
+    // Source: https://csharpindepth.com/articles/singleton
+    public sealed class Singleton
+    {
+        private static readonly Singleton _instance = new Singleton();
+
+        // reading this will initialize the _instance
+        public static readonly string GREETING = "Hi!";
+
+        // Tell C# compiler not to mark type as beforefieldinit
+        // (https://csharpindepth.com/articles/BeforeFieldInit)
+        static Singleton()
+        {
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                Logger.Log("Instance called.");
+                return _instance;
+            }
+        }
+
+        private Singleton()
+        {
+            // cannot be created except within this class
+            Logger.Log("Constructor invoked.");
+        }
+    }
+}
